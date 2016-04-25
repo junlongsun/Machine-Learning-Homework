316ef0cf09b3505af3925617b15c0726e24952cb
junlongsun <Leo@dafgsfdfgfadfasdasdas.local>
2016-04-24 19:03:02 -0600

    finish exact NFL data

--- project/exactDatafromPython.py
+++ project/exactDatafromPython.py
@@ -15,9 +15,9 @@ mc = 10
 #training
 #data2: combine 2014, first 80% for train, 20% for test
 data2013 = list(DictReader(open("pbp-2013.csv", 'r')))
-#data2014 = list(DictReader(open("pbp-2014.csv", 'r')))
-#data2015 = list(DictReader(open("pbp-2015.csv", 'r')))
-#dataList = [data2013, data2014, data2015]
+data2014 = list(DictReader(open("pbp-2014.csv", 'r')))
+data2015 = list(DictReader(open("pbp-2015.csv", 'r')))
+dataList = [data2013, data2014, data2015]
 dataName = ["2013","2014","2015"]
 
 #o = DictWriter(open("rfClassifier-mc.csv", 'w'), ["dataName", "classifier", "percent", "score", "OmniScore", "Type1-A/A/Good","Type2-A/B/Bad",  "Type3-A/B/Good", "Type4-A/A/Bad"])
@@ -26,10 +26,17 @@ csvfile = ["2013X.csv","2013Y.csv","2014X.csv","2014Y.csv","2015X.csv","2015Y.cs
 
 num = -1
 #---------------------------------#
-pbp2014 = NewPbpExtractor()
-pbp2014.buildFormationList(data2013)
-feature, target = pbp2014.extract4Classifier(data2013)
+for dataindex in range(len(dataList)):
+    pbp2014 = NewPbpExtractor()
+    pbp2014.buildFormationList(dataList[dataindex])
+    feature, target = pbp2014.extract4Classifier(dataList[dataindex])
 
-with open(csvfile[1], "w") as output:
+    num += 1
+    with open(csvfile[num], "w") as output:
         writer = csv.writer(output, lineterminator='\n')
-        writer.writerows(target)
+        writer.writerows(feature)
+
+    num += 1
+    with open(csvfile[num], "w") as output:
+        writer = csv.writer(output, lineterminator='\n')
+        writer.writerow(target)
